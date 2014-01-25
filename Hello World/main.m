93e16145fcce34a64f2c81920bde9ae4a60055b8
David McLaren <sirusblk@gmail.com>
2014-01-24 19:08:21 -0800

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by David McLaren on 1/24/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
