0411a01d7ddf43e5a6cd3c366a6d342722da7cfb
David McLaren <sirusblk@gmail.com>
2014-01-26 20:05:54 -0800

    Removed tracked files. Replaced others.

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,45 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by David McLaren on 1/24/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+		printf("Please enter in a message:\n");
+		
+		// Doesn't quite work, can't tell end of input
+		/*
+		NSFileHandle *console = [NSFileHandle fileHandleWithStandardInput];
+		NSData *inputData = [NSData dataWithData:[console readDataToEndOfFile]];
+		NSString *inputString = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
+		 */
+		
+		//NSString *inputString = scanf(@"%@", inputString);
+		
+		// Spit out a UTF-8 formated string using printf (stdout)
+		//printf("%s", [inputString UTF8String]);
+		
+		// Output current date and time - WORKS!
+		NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+		[dateFormat setDateFormat:@"MM/dd/yyyy, HH:mm"];
+		NSString *outputDate = [dateFormat stringFromDate:[NSDate date]];
+		printf("%s", [outputDate UTF8String]);
+		printf("\n");
+	}
+    return 0;
+}
+
+/*
+ What is the name and version of the compiler used?		Clang
+ Where is your project file and source code stored?		In my project folder...
+ Where are the intermediate files stored?
+ Where is the application bundle stored?
+ Where is the project's binary executable stored?
+ */
