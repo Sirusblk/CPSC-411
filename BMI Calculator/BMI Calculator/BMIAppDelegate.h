bf25f6add0ef20e09f12254b5a0f1526a23d6ca2
David McLaren <sirusblk@gmail.com>
2014-02-07 15:12:45 -0800

    Updated BMI Calculator

---
+++ BMI Calculator/BMI Calculator/BMIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BMIAppDelegate.h
+//  BMI Calculator
+//
+//  Created by David McLaren on 2/2/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BMI Calculator/BMI Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BMI Calculator
+//
+//  Created by David McLaren on 2/2/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BMIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BMIAppDelegate class]));
+	}
+}
