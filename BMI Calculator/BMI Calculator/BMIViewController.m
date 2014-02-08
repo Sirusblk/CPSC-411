b0661a0758f82f9a66dc3a8e306c6c0eb3676d3f
David McLaren <sirusblk@gmail.com>
2014-02-07 19:40:03 -0800

    Calculation now works for metric units.

--- BMI Calculator/BMI Calculator/BMIViewController.m
+++ BMI Calculator/BMI Calculator/BMIViewController.m
@@ -39,18 +39,48 @@
 - (IBAction)calculatePressed:(id)sender {
 	NSLog(@"Calculate Button Pressed.");
 	
-	/*
-	NSString *t = self.myLabel.text;
-	int x = [t intValue];
-	x += 1;
-	t = [NSString stringWithFormat:@"%d", x];
-	self.myLabel.text = t;
-	 */
-	
-	NSNumber *rangeBMI = [NSNumber alloc];
 	NSString *categoryBMI = [NSString alloc];
+    float rangeBMI;
+    float heightBMI;
+    float weightBMI;
+    
+    //Check for Metric Units toggle, convert to metric!
+    if (self.metricSwitchOutlet.isOn) {
+        //Nothing changes, already in metric
+        heightBMI = [self.heightTextField.text integerValue] / 100.00;
+        NSLog(@"Height: %2.2f m", heightBMI);
+        weightBMI = [self.weightTextField.text integerValue];
+        NSLog(@"Weight: %2.2f kg", weightBMI);
+    } else {
+        //Must convert to metric, change below!
+        heightBMI = 0;
+        weightBMI = 0;
+    }
+    
+    //Calculate BMI Range
+    rangeBMI = (weightBMI / (heightBMI * heightBMI));
+    NSLog(@"%2.2f", rangeBMI);
+    
+    //Calculate BMI Category
+    if (rangeBMI < 16.00) {
+        categoryBMI = @"Severe Thinness";
+    } else if (rangeBMI < 17.00) {
+        categoryBMI = @"Moderate Thinness";
+    } else if (rangeBMI < 18.50) {
+        categoryBMI = @"Mild Thinness";
+    } else if (rangeBMI < 25.00) {
+        categoryBMI = @"Normal Range";
+    } else if (rangeBMI < 30.00) {
+        categoryBMI = @"Overweight";
+    } else if (rangeBMI < 35.00) {
+        categoryBMI = @"Obese Class I (Moderate)";
+    } else if (rangeBMI < 40.00) {
+        categoryBMI = @"Obese Class II (Severe)";
+    } else {
+        categoryBMI = @"Obese Class III (Very Severe)";
+    }
 	
-	self.resultLabel.text = [NSString stringWithFormat:@"%@ %@", rangeBMI, categoryBMI];
+	self.resultLabel.text = [NSString stringWithFormat:@"%2.2f %@", rangeBMI, categoryBMI];
 }
 
 - (IBAction)metricSwitch:(id)sender {
