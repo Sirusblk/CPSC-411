93762c3084264e08822af80e60d691471b5d5cca
David McLaren <sirusblk@gmail.com>
2014-03-17 23:04:59 -0700

    Added Calculator Data model.

---
+++ Calculator/Calculator/CalculatorData.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorData.h
+//  Calculator
+//
+//  Created by David McLaren on 3/17/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorData : NSObject
+
+@end
---
+++ Calculator/Calculator/CalculatorData.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorData.m
+//  Calculator
+//
+//  Created by David McLaren on 3/17/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import "CalculatorData.h"
+
+@implementation CalculatorData
+
+@end
