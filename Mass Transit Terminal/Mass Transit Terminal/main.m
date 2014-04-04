ca27cbf471fa397cbb4311c4b8782d762792eb46
David McLaren <sirusblk@gmail.com>
2014-04-04 10:43:46 -0700

    Initial Commit.

---
+++ Mass Transit Terminal/Mass Transit Terminal/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Mass Transit Terminal
+//
+//  Created by David McLaren on 4/4/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
