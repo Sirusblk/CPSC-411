68d00b4f59918168347581363065ca120e9b0e68
David McLaren <sirusblk@gmail.com>
2014-04-04 12:20:46 -0700

    Initial Commit.

---
+++ Mass Transit/Mass Transit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mass Transit
+//
+//  Created by David McLaren on 4/4/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mass Transit/Mass Transit/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Mass Transit
+//
+//  Created by David McLaren on 4/4/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
