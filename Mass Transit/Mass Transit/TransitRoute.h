c581d14e282f630f62aea4c345c879ef562df4de
David McLaren <sirusblk@gmail.com>
2014-04-05 15:19:31 -0700

    Added databases and TransitRoute object.

---
+++ Mass Transit/Mass Transit/TransitRoute.h
@@ -0,0 +1,13 @@
+//
+//  TransitRoute.h
+//  Mass Transit
+//
+//  Created by David McLaren on 4/5/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TransitRoute : NSObject
+
+@end
