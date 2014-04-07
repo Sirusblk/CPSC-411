a2ca47149f6035928cb2b7fee88f2506972946bc
David McLaren <sirusblk@gmail.com>
2014-04-07 00:13:46 -0700

    Added stop location object.

---
+++ Mass Transit/Mass Transit/StopLocation.h
@@ -0,0 +1,13 @@
+//
+//  StopLocations.h
+//  Mass Transit
+//
+//  Created by David McLaren on 4/6/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StopLocations : NSObject
+
+@end
---
+++ Mass Transit/Mass Transit/StopLocation.m
@@ -0,0 +1,13 @@
+//
+//  StopLocations.m
+//  Mass Transit
+//
+//  Created by David McLaren on 4/6/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import "StopLocations.h"
+
+@implementation StopLocations
+
+@end
