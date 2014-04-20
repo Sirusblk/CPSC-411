8f2189fd5ed7aa9efe120cd5b35260e4d95289d1
David McLaren <sirusblk@gmail.com>
2014-04-20 16:34:35 -0700

    Initial commit

---
+++ SenderTest/SenderTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SenderTest
+//
+//  Created by David McLaren on 4/17/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SenderTest/SenderTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SenderTest
+//
+//  Created by David McLaren on 4/17/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
