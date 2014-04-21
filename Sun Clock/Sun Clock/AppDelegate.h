29cb58e36294fc98570e06463819d83e0b870cad
David McLaren <sirusblk@gmail.com>
2014-04-20 17:22:35 -0700

    Initial commit

---
+++ Sun Clock/Sun Clock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sun Clock
+//
+//  Created by David McLaren on 4/20/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
