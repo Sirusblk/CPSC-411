d6eea4b439d9ab3d4e7f5d71c930b35dd6140e7b
David McLaren <sirusblk@gmail.com>
2014-04-24 21:21:11 -0700

    Created iOS version of LibNova library

---
+++ iOS-Nova/iOS-Nova/iOS_Nova.m
@@ -0,0 +1,13 @@
+//
+//  iOS_Nova.m
+//  iOS-Nova
+//
+//  Created by David McLaren on 4/23/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import "iOS_Nova.h"
+
+@implementation iOS_Nova
+
+@end
