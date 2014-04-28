08396372e4d21b32796acbd09adadcc4598a5741
David McLaren <sirusblk@gmail.com>
2014-04-27 23:23:12 -0700

    Added test view

---
+++ Sun Clock/Sun Clock/ViewControllers/TestClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestClockViewController.h
+//  Sun Clock
+//
+//  Created by David McLaren on 4/27/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestClockViewController : UIViewController
+
+@end
