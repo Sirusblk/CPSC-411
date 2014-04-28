ebb81b26056df91ea6d006ecc2aa17c754f8db65
David McLaren <sirusblk@gmail.com>
2014-04-27 19:53:08 -0700

    Added resulting clock view and view controller

---
+++ Sun Clock/Sun Clock/Views/SunClockView.h
@@ -0,0 +1,13 @@
+//
+//  SunClockView.h
+//  Sun Clock
+//
+//  Created by David McLaren on 4/27/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SunClockView : UIView
+
+@end
---
+++ Sun Clock/Sun Clock/Views/SunClockView.m
@@ -0,0 +1,31 @@
+//
+//  SunClockView.m
+//  Sun Clock
+//
+//  Created by David McLaren on 4/27/14.
+//  Copyright (c) 2014 David McLaren. All rights reserved.
+//
+
+#import "SunClockView.h"
+
+@implementation SunClockView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
