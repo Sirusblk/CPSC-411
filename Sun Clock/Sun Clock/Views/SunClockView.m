7f48178a45b2296a7cc10b25baaaa0de9262743e
David McLaren <sirusblk@gmail.com>
2014-05-11 02:42:46 -0700

    Pie slice now renders

--- Sun Clock/Sun Clock/Views/SunClockView.m
+++ Sun Clock/Sun Clock/Views/SunClockView.m
@@ -24,19 +24,39 @@
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
-    double circleSize = 250;
+    double circleSize = 200;
+    CGPoint circleCenter = CGPointMake(self.frame.size.width/2.0, self.frame.size.height/2.0);
     CGContextRef context = UIGraphicsGetCurrentContext();
     
     // Draw Background
     CGContextSetRGBFillColor(context, 71/255.0f, 83/255.0f, 94/255.0f, 1.0);
-    CGContextFillRect(context, CGRectMake(0.0, 0.0, 320.0, 430.0));
+    CGContextFillRect(context, CGRectMake(0.0, 0.0, 320.0, 500.0));
+    
+    // Draw Shadow
+    CGContextSaveGState(context);
+    CGContextSetShadow(context, CGSizeMake(0, 0), 8);
     
     // Draw Circle
-    CGRect frame = CGRectMake(self.center.x - (circleSize / 2), self.center.y - (circleSize / 2), circleSize, circleSize);
+    CGRect circleframe = CGRectMake(circleCenter.x - (circleSize / 2), circleCenter.y - (circleSize / 2), circleSize, circleSize);
     CGContextSetRGBFillColor(context, 50/255.0f, 65/255.0f, 82/255.0f, 1.0);
-    CGContextFillEllipseInRect(context, frame);
+    CGContextFillEllipseInRect(context, circleframe);
+    
+    // Restore State for drop shadow
+    CGContextRestoreGState(context);
     
     // Draw Dawn to Sunrise
+    double origin = M_PI / 2;
+    double start = origin + ((4.0 / 24.0 * 360) * (M_PI / 180));
+    double end = origin + (5.0 / 24.0 * 360) * (M_PI / 180);
+
+    NSLog(@"Origin: %f", origin);
+    NSLog(@"Start:  %f", start);
+    NSLog(@"End:    %f", end);
+    
+    CGContextSetRGBFillColor(context, 121/255.0f, 100/255.0f, 102/255.0f, 1.0);
+    CGContextMoveToPoint(context, circleCenter.x, circleCenter.y);
+    CGContextAddArc(context, circleCenter.x, circleCenter.y, circleSize / 2, start, end, 0);
+    CGContextFillPath(context);
     
     // Draw Sunrise to Sunset
     
